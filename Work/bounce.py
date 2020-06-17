4df715896780fb3f8d4663a2ca5731774833ab1b
Kristian Sneskov <kso@beumergroup.com>
2020-06-17 21:03:36 +0200

    Completed part 1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,18 @@
 # bounce.py
 #
 # Exercise 1.5
+
+import math as math
+
+decay_rate = 0.6 # 3/5 of previous rate
+initial_height = 100 # Height (meters)
+bounce_threshold = 10
+bounce_no = 1
+
+while bounce_no <= bounce_threshold:
+    print(bounce_no, round(initial_height * math.pow(decay_rate,bounce_no),4))
+    bounce_no = bounce_no + 1
+
+#print('Number of days', day)
+#print('Number of bills', num_bills)
+#print('Final height', num_bills * bill_thickness)
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,28 @@
 # pcost.py
 #
 # Exercise 1.27
+
+
+import os
+import csv
+
+def portfolio_cost(filename):
+    try:
+        with open(filename, 'rt') as f:
+            rows = csv.reader(f)
+            header = next(rows)
+            sum = 0
+            for row in rows:
+                (name,qty,price) = row
+                try:
+                    sum = sum + int(qty) * float(price)
+                except ValueError:
+                    print(f'invalid record encountered on line:{row} - skipping entry')
+        return sum
+    except FileNotFoundError:
+        print('File not found')
+        return None
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
+
