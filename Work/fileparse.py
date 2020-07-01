4e038a3e53a34a8bebbeae074f2baace83a4a117
Kristian Sneskov <kso@beumergroup.com>
2020-07-01 08:45:27 +0200

    implementation of column selection logic
    add type casting
    
    has_header option support
    
    add support for custom delimiter
    
    Exercise 3.11

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -6,20 +6,45 @@
 
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename, select = [], types = [], has_headers = True, delimiter = ',', silence_errors=False):
     '''
     Parse a CSV file into a list of records
     '''
+    if select and not has_headers:
+        raise RuntimeError("select argument requires column headers")
     with open(filename) as f:
-        rows = csv.reader(f)
-
+        rows = csv.reader(f, delimiter=delimiter)
         # Read the file headers
-        headers = next(rows)
+        if has_headers:
+            headers = next(rows)
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = []
+        else:
+            indices = []
+
         records = []
-        for row in rows:
+        for index,row in enumerate(rows, start=1):
             if not row:    # Skip rows with no data
                 continue
-        record = dict(zip(headers, row))
-        records.append(record)
+            if indices:
+                row = [ row[index] for index in indices]
+            #row = [ row[index] for index in indices]
+
+            if types:
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except ValueError as e:
+                    if not silence_errors:
+                        print(f"Row {index}: Could not convert {row}")
+                        print(f"Row {index}: {e}")
+            #record = {colname: row[index] for colname, index in zip(select, indices)}
+            if has_headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+            records.append(record)
 
     return records
